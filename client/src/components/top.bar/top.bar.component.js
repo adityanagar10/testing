2451a1b24247cc7d496f49ecc4d34ac3608daad2
Aditya Nagar <nagar.s.aditya@gmail.com>
2021-07-21 09:59:40 +0530

    top bar

--- client/src/components/top.bar/top.bar.component.js
+++ client/src/components/top.bar/top.bar.component.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import './top.bar.styles.scss'
 
 
 export const TopBar = () => {
     return(
-        <div>
-            Wallpaper App
+        <div className="testing">
+            <div className="topBarContainer">
+           <div className="topBarLogo">
+               <h1> - Logo -</h1>
+           </div>
+           <div className="topBarItems">
+               <h1>
+                   Log In with Discord
+               </h1>
+           </div>
+        </div>
         </div>
     )
 }
